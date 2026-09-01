04d941da33c2209fb4b834b1d909585458037ff2
moinkmu <m.a.khaja.moinuddin@accenture.com>
2026-09-01 22:47:32 +0530

    save

---
+++ ConsoleDoverApp/Class8.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace ConsoleDoverApp
+{
+    internal class Class8
+    {
+        public void test() {
+            Console.WriteLine("test ex stash");
+        }
+    }
+}
---
+++ ConsoleDoverApp/Stashex.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace ConsoleDoverApp
+{
+    internal class Stashex
+    {
+        public void test() {
+            Console.WriteLine("stash");
+        }
+
+        public void Test2() { 
+        
+        }
+    }
+}
