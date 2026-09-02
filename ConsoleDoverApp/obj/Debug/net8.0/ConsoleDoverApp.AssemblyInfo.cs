9440ef5cee25b83117269fa5294fad0446f8a59a
moinkmu <m.a.khaja.moinuddin@accenture.com>
2026-09-02 11:33:00 +0530

    reset1

--- ConsoleDoverApp/obj/Debug/net8.0/ConsoleDoverApp.AssemblyInfo.cs
+++ ConsoleDoverApp/obj/Debug/net8.0/ConsoleDoverApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleDoverApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+753878eca33e24ff90a9adc7d93eed07f4120f5a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b26a97e067a5c8fc8218501d36d4b4bade9bb7aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleDoverApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleDoverApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
